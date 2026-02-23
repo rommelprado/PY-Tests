65a818dc73137c33f30754c3398a2d2facf8d2b9
rommelprado <128178633+rommelprado@users.noreply.github.com>
2026-02-22 23:55:52 -0300

    Update Planilha.py

--- Planilha.py
+++ Planilha.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
+import streamlit.components.v1 as components
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
